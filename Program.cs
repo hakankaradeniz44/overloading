624041067460a5bd3f7d63b697fd3f47239b7b0a
Semihck <semih.kartal@lcwaikiki.com>
2025-11-27 14:56:14 +0300

    overloading

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       string sayi = "999";
+       bool sonuc = int.TryParse(sayi, out int outSayi);
+       if (sonuc)
+       {
+           Console.WriteLine("Başarılı!");
+           Console.WriteLine(outSayi);  
+    }
+         else
+         {
+              Console.WriteLine("Başarısız!");
+         }
+
+         Metotlar instance = new Metotlar();
+            instance.Topla(4,5, out int toplam);
+            Console.WriteLine(toplam);
+            instance.EkranaYazdir(Convert.ToString(toplam));
+
+            
+
+     }
+
+     class Metotlar
+     {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+            Console.WriteLine("Toplam: " + toplam);
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+         
+     }
+}
